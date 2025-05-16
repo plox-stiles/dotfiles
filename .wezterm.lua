137b92880fed90333831c3c55735b7a1e0238499
plox <user@plox>
2025-05-16 15:41:45 -0500

    changed colors

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,25 +1,28 @@
+-- Pull in the wezterm API
 local wezterm = require("wezterm")
+
+-- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- font settings
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
 config.font_size = 10
 config.font = wezterm.font({
 	family = "FiraCode Nerd Font",
 	weight = "Medium",
 })
 
+config.color_scheme = "flexoki-dark"
+
 -- shell
 config.default_prog = { "pwsh" }
-config.color_scheme = "nordfox"
-config.initial_cols = 120
-config.initial_rows = 30
-config.default_cursor_style = "SteadyBlock"
-
--- window decoration
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.92
--- system bell
 config.audible_bell = "Disabled"
 config.visual_bell = {
 	fade_in_function = "EaseIn",
@@ -30,5 +33,7 @@ config.visual_bell = {
 config.colors = {
 	visual_bell = "#202020",
 }
-
+config.window_background_opacity = 1.00
+config.color_scheme_dirs = { "~/.colors" }
+-- Finally, return the configuration to wezterm:
 return config
